06de1a2abd6b9960808448d2a37c39472e200806
김영은 <95126m@naver.com>
2024-07-30 15:02:24 +0900

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,41 @@
-import { useState, useEffect } from 'react'
-export interface Root {
-  total: number
-  users: User[]
-}
-
-export interface User {
-  id: string
-  name: string
-  age: number
-  isValid: boolean
-  emails: string[]
-  photo?: Photo
-}
-
-export interface Photo {
-  name: string
-  size: number
-  mimeType: string
-  url: string
-}
+import { useState, useEffect, Fragment } from 'react'
 import TheLoader from './components/TheLoader'
+import UserItem from './components/TodoItem'
 
-export default function App() { // 데이터, useState = Hook
-  const [users, setUsers] = useState<User[]>([]) //never[]
-  const [count, setCount] = useState(31)
-  const [message, setMessage] = useState('')
+export type Todos = Todo[]
+export interface Todo {
+  id: string
+  order: number
+  title: string
+  done: boolean
+  createdAt: string
+  updatedAt: string
+}
+export default function App() { 
+  const [todos, setTodos] = useState<Todos>([]) 
+  const [message, setMessage] = useState('') 
   const [loading, setLoading] = useState(true)
-  function increase() {
-    setCount(count + 1)
-  }
   // App 컴포넌트가 준비되었을 때 (Mount, 최초 랜더링), useEffect(콜백, 종속성배열- 의존성배열)
   useEffect(() => {
-    getUsers()
+    getTodos()
   }, [])
-  async function getUsers() { // 사용자를 가져와라
+  // 사용자를 가져와라
+  async function getTodos() { 
     try {
-      await new Promise(resolve => setTimeout(resolve, 3000)) // 3초 기다린 후 실행
-      const res = await fetch('https://api.heropy.dev/v0/users')
+      await new Promise(resolve => setTimeout(resolve, 2000))
+      const res = await fetch(
+        'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos', 
+        {
+        headers: { 
+            'content-type': 'application/json',
+            apikey: '5X8Z1k7M2vU5Q',
+            username: 'Grepp_KDT4_ParkYoungWoong'
+          }
+        }
+      )
       const data = await res.json()
       console.log('응답결과', data)
-      setUsers(data.users)
+      setTodos(data)
     } catch (error) {
       if (error instanceof Error) {
         const message = '서버가 폭발했어요'
@@ -50,17 +46,24 @@ export default function App() { // 데이터, useState = Hook
       setLoading(false)
     }
   }
-  return ( // 화면    
+  // 화면
+  return (   
     <>
-      <div>{count}명</div>
-      <button onClick={increase}>증가</button>
       <div>{loading && <TheLoader />}</div>
       <div>{message}</div>
       <ul>
-        {users.map(user => (
-          <li key={user.name}>{user.name}</li>
+        {todos.map(todo => (
+          <Fragment key={todo.id}>
+            <UserItem abc={todo} />
+          </Fragment>
         ))}
       </ul>
     </>
   )
 }
+
+// fetch('주소', {
+//   headers: {},
+//   body: {},
+//   method: 'GET'
+// })
